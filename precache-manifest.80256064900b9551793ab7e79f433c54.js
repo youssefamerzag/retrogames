2b0b7c0075eeb8cf2c918f28edf1ab8560b71b80
youssefame <amerzag.youssef4@gmail.com>
2022-12-05 00:37:54 +0100

    Updates

---
+++ precache-manifest.80256064900b9551793ab7e79f433c54.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "a485687562aff2fbc7d0",
+    "url": "/react_upload/static/css/main.bd202a1b.chunk.css"
+  },
+  {
+    "revision": "a485687562aff2fbc7d0",
+    "url": "/react_upload/static/js/main.a4856875.chunk.js"
+  },
+  {
+    "revision": "1c78b0d78cd59a14a5c6",
+    "url": "/react_upload/static/js/1.1c78b0d7.chunk.js"
+  },
+  {
+    "revision": "1f7efc12cdeba9e3d821",
+    "url": "/react_upload/static/js/runtime~main.1f7efc12.js"
+  },
+  {
+    "revision": "e100c723ca8bebcf8ed4a6849e9c702f",
+    "url": "/react_upload/static/media/ps5.e100c723.jpg"
+  },
+  {
+    "revision": "e0e52e19a7349c8df4afb5d86dc81368",
+    "url": "/react_upload/static/media/switch.e0e52e19.jpg"
+  },
+  {
+    "revision": "bb986040739d24bac9f0a2da2d35f91a",
+    "url": "/react_upload/static/media/3ds.bb986040.jpg"
+  },
+  {
+    "revision": "73ddb2b8ca98c65d5ec535ed24634803",
+    "url": "/react_upload/static/media/xbox.73ddb2b8.large"
+  },
+  {
+    "revision": "43b5856241e4482bfeccb5832f87fd97",
+    "url": "/react_upload/static/media/wiiu.43b58562.jpg"
+  },
+  {
+    "revision": "1bfd89c89ec0a6c809506659bcf44ac6",
+    "url": "/react_upload/static/media/micro.1bfd89c8.jpg"
+  },
+  {
+    "revision": "49384e15fb1d44d6702607ada9ae6bea",
+    "url": "/react_upload/static/media/ps4.49384e15.jpg"
+  },
+  {
+    "revision": "60d30281eaf0ba493c12885a3688467b",
+    "url": "/react_upload/static/media/psvita.60d30281.jpg"
+  },
+  {
+    "revision": "033903129c7f8b6b7f7e32dbc0b35c52",
+    "url": "/react_upload/static/media/psp.03390312.jpg"
+  },
+  {
+    "revision": "22e78e0373fb4c62841654cf620441d4",
+    "url": "/react_upload/static/media/gb.22e78e03.webp"
+  },
+  {
+    "revision": "ab307d1bb136967bec74a7c815f1b302",
+    "url": "/react_upload/static/media/ds.ab307d1b.jpg"
+  },
+  {
+    "revision": "6ff89a2e54180538105e5f9846c5809b",
+    "url": "/react_upload/static/media/snes.6ff89a2e.jpg"
+  },
+  {
+    "revision": "e611491183b75507dddb611d970d1cca",
+    "url": "/react_upload/static/media/n64.e6114911.jpg"
+  },
+  {
+    "revision": "974fcd774c2804ce5daa42c9c84b9f15",
+    "url": "/react_upload/index.html"
+  }
+];
